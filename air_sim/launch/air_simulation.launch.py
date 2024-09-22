5aab8b9918bc01c51a020bf95269643c42bf4d76
victormatteus04 <victormatteus@distente.ufg.br>
2024-09-21 21:51:04 -0300

    Some improvements to the ufg.world world; Added option to specify the the position to spawn the vehicle at the start of the simulation.

--- air_sim/launch/air_simulation.launch.py
+++ air_sim/launch/air_simulation.launch.py
@@ -3,7 +3,7 @@ from launch_ros.substitutions import FindPackageShare
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution, LaunchConfiguration, Command, PythonExpression
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration, PythonExpression
 from launch.conditions import IfCondition
 import os
 import xacro
@@ -11,6 +11,7 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     
+    # Launch argument for RViz
     rviz_arg = DeclareLaunchArgument(
         'rviz',
         default_value='false',
@@ -20,13 +21,22 @@ def generate_launch_description():
     share_dir = get_package_share_directory('air_description')
     rviz_dir = get_package_share_directory('air_sim')
 
-    world_name = LaunchConfiguration('world_name', default='default.world')
+    # Launch argument for world file
+    world_name = LaunchConfiguration('world_name', default='ufg.world')
     declare_world_name_arg = DeclareLaunchArgument(
         'world_name',
-        default_value= world_name,
+        default_value=world_name,
         description='Name of the .world file to load'
     )
     
+    # Declare a single argument for position and orientation (as a vector or tuple)
+    declare_robot_pose_arg = DeclareLaunchArgument(
+        'robot_pose',
+        default_value="6.0, -1.0, 0.3, 0.0, 0.0, 0.0",
+        description='Initial position and orientation of the robot in the format: x, y, z, roll, pitch, yaw'
+    )
+
+    # Path to world file
     custom_world_file = PathJoinSubstitution([
         get_package_share_directory('air_sim'),
         'worlds',
@@ -36,9 +46,11 @@ def generate_launch_description():
     rviz_file = os.path.join(rviz_dir, 'config', 'air.rviz')
     xacro_file = os.path.join(share_dir, 'urdf', 'sd_twizy.urdf.xacro')
 
+    # Process the xacro file to get the robot's URDF description
     robot_description_config = xacro.process_file(xacro_file)
     robot_urdf = robot_description_config.toxml()
     
+    # Nodes
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -76,16 +88,22 @@ def generate_launch_description():
         ])
     )
 
+    # Modify the urdf_spawn_node to include position and orientation parameters
     urdf_spawn_node = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
         arguments=[
             '-entity', 'sd_twizy',
             '-topic', 'robot_description',
+            '-x', PythonExpression(["'", LaunchConfiguration('robot_pose'), "'.split(',')[0]"]),
+            '-y', PythonExpression(["'", LaunchConfiguration('robot_pose'), "'.split(',')[1]"]),
+            '-z', PythonExpression(["'", LaunchConfiguration('robot_pose'), "'.split(',')[2]"]),
+            '-R', PythonExpression(["'", LaunchConfiguration('robot_pose'), "'.split(',')[3]"]),
+            '-P', PythonExpression(["'", LaunchConfiguration('robot_pose'), "'.split(',')[4]"]),
+            '-Y', PythonExpression(["'", LaunchConfiguration('robot_pose'), "'.split(',')[5]"])
         ],
         output='screen',
     )
-
 
     rviz2_node = Node(
         package='rviz2',
@@ -102,6 +120,8 @@ def generate_launch_description():
 
     return LaunchDescription([
         rviz_arg,
+        declare_world_name_arg,
+        declare_robot_pose_arg,
         robot_state_publisher_node,
         joint_state_publisher_node,
         gazebo_server,
